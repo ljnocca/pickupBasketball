e198b6de5268ddd16db7aaa9c275c6c2fdccbe50
Laurent Nocca <lnocca@fanreact.com>
2017-12-01 22:10:02 -0600

    correctly displaying oninit

--- src/app/next-game/calendar/calendar.component.ts
+++ src/app/next-game/calendar/calendar.component.ts
@@ -10,6 +10,7 @@ export class CalendarComponent implements OnInit {
 
   daysAdded: number;
   gameDay;
+  gameTime;
 
   constructor() { }
 
@@ -18,19 +19,25 @@ export class CalendarComponent implements OnInit {
 
     if (today === '7') {
       this.daysAdded = 2;
+      this.gameTime = '6:30 AM';
     } else if (today === '1') {
       this.daysAdded = 1;
+      this.gameTime = '6:30 AM';
     } else if (today === '2') {
       this.daysAdded = 0;
+      this.gameTime = '6:30 AM';
     } else if (today === '3') {
       this.daysAdded = 1;
+      this.gameTime = '6:30 AM';
     } else if (today === '4') {
       this.daysAdded = 0;
+      this.gameTime = '6:30 AM';
     } else if (today === '5') {
-      console.log('YESS');
       this.daysAdded = 1;
+      this.gameTime = '7:30 AM';
     } else if (today === '6') {
       this.daysAdded = 0;
+      this.gameTime = '7:30 AM';
     }
 
     this.gameDay = moment().add(this.daysAdded, 'days').format('dddd, MMMM Do');
--- src/app/next-game/next-game.component.ts
+++ src/app/next-game/next-game.component.ts
@@ -13,6 +13,7 @@ import * as firebase from 'firebase';
 export class NextGameComponent implements OnInit, OnChanges {
   public nextGame;
   public loggedInEmail;
+  public loggedInUserIndex;
   public status = 'OUT';
   public checked;
   public players: Array<Player> = [];
@@ -25,57 +26,66 @@ export class NextGameComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     this.token = this.authService.getToken();
-    console.log(this.checked);
-    console.log(this.status);
+    console.log('checked', this.checked);
+    console.log('status', this.status);
     this.http.get('https://pickupbasketball-11fc7.firebaseio.com/players.json?auth=' + this.token)
       .subscribe(
         (response: Response) => {
-          if (response.json() !== null) {
-            // if players already exist, set the player array to what is already stored in Firebase
-            this.players = response.json();
+          // set players array to response
+          this.players = response.json();
+          this.loggedInEmail = firebase.auth().currentUser.email;
+
+          // find index of logged in user to set status
+          for (let i = 0; i < this.players.length; i++) {
+            if (this.loggedInEmail === this.players[i].email) {
+              this.loggedInUserIndex = i;
+              this.status = this.players[this.loggedInUserIndex].status;
+            }
           }
+
+          // set toggle
+          if (this.status === 'IN') {
+            this.checked = true;
+          } else if (this.status === 'OUT') {
+            this.checked = false;
+          }
+          // create the IN and OUT array for display
           for (let i = 0; i < this.players.length; i++) {
             if (this.players[i].status === 'IN') {
-              this.checked = true;
               this.playersIn.push(this.players[i]);
             } else if (this.players[i].status === 'OUT') {
-              this.checked = false;
               this.playersOut.push(this.players[i]);
             }
           }
-          console.log(this.checked);
-          console.log(this.status);
+          console.log('checked', this.checked);
+          console.log('status', this.status);
         }
       );
-    this.loggedInEmail = firebase.auth().currentUser.email;
-    console.log('email', this.loggedInEmail);
   }
 
   ngOnChanges() {
-    for (let i = 0; i < this.players.length; i++) {
-      if (this.players[i].status === 'IN') {
-        this.playersIn.push(this.players[i]);
-      } else if (this.players[i].status === 'OUT') {
-        this.playersOut.push(this.players[i]);
-      }
-    }
+    // for (let i = 0; i < this.players.length; i++) {
+    //   if (this.players[i].status === 'IN') {
+    //     this.playersIn.push(this.players[i]);
+    //   } else if (this.players[i].status === 'OUT') {
+    //     this.playersOut.push(this.players[i]);
+    //   }
+    // }
   }
 
   onSlide() {
     if (this.status === 'OUT' ) {
-      this.checked = true;
+      this.checked = false;
       this.status = 'IN';
     } else if (this.status === 'IN') {
-      this.checked = false;
+      this.checked = true;
       this.status = 'OUT'
     }
 
-    let loggedInUserIndex;
     for (let i = 0; i < this.players.length; i++) {
       console.log('player ' + i + this.players[i]);
       if (this.loggedInEmail === this.players[i].email) {
-        loggedInUserIndex = i;
-        this.players[loggedInUserIndex].status = this.status;
+        this.players[this.loggedInUserIndex].status = this.status;
       }
     }
 
